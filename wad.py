31d606fcb832508e83984e3c53eef87cbb24438a
Edmund Huber <me@ehuber.info>
2018-10-04 23:31:45 -0700

    pretty big rewrite; 'wad init' works again

--- wad.py
+++ wad.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python2
 import base64
-import glob
+import fcntl
 import hashlib
 import inspect
 import os.path
 import re
 import shutil
 import sys
+import tempfile
 
 # TODO: pepwhatever, pyflakes
 
@@ -33,10 +34,11 @@ def command_help(*args):
 
 def goto(reference):
     # The reference must not be broken.
-    commit = look_up_commit(reference)
-    # The repository must be clean.
-    if len(diff()) > 0:
-        raise UsageException("Can't goto when there are un-committed or stashed changes.")
+    commit = WadObject.look_up(reference)
+    # The repository must be clean. TODO
+    #if len(diff()) > 0:
+    #    raise UsageException("Can't goto when there are un-committed or stashed changes.")
+    # TODO check out the actual files.
     # Store the new head.
     head_fn = os.path.join('.wad', 'head')
     try:
@@ -45,60 +47,145 @@ def goto(reference):
     except IOError:
         raise UsageException("Broken repository - {} can't be written to!".format(head_fn))
 
+# TODO: multi-tenancy doesn't work.
 
-# TODO: multi-tenancy doesn't work. lack of any lockfile means that wad operations can't be interleaved
-# TODO: also no way to recover if an operation dies halfway - have a .wad/mirror directory that gets merged in at end?
-# or keep multiple directories and point at the current one?
+class WadObjectRegistry(type):
+
+    registry = []
+
+    def __new__(cls, name, bases, attrs):
+        new_cls = type.__new__(cls, name, bases, attrs)
+        cls.registry.append(new_cls)
+        return new_cls
+
+    @classmethod
+    def get(cls):
+        return cls.registry
+
 
 class WadObject(object):
 
-    # use abc.abstract?
+    _type = None
+
+    def __init__(self, reference):
+        self._stage_dir_and_flock = None
+        self._reference = reference # TODO: sub_ref, because not canonical like commit/323342
+        if reference is None:
+            self._set_up_stage()
+
+    @classmethod
+    def look_up(cls, reference):
+        for registered_cls in WadObjectRegistry.get():
+            prefix = registered_cls._type + '/' # TODO should be one place to build this
+            if reference.startswith(prefix):
+                return registered_cls(reference[len(prefix):])
+        return None
+
+    def does_exist(self):
+        return self._reference is None
+
     def get_reference(self):
-        raise UnreachableException()
+        if self._reference is None:
+            raise Exception() # TODO internalexceptoin
+        return self._type + '/' + self._reference
 
-    @classmethod
-    def _filename(cls, reference):
-        return os.path.join('.wad', reference)
+    def _reference_dir(self):
+        return os.path.join('.wad', self._type, self._reference)
 
-    @classmethod
-    def load(cls, reference):
-        cls._check_reference(reference)
-        if not os.path.exists(cls._filename(reference)):
-            return None
-        with open(cls._filename(reference)) as f:
-            _object = cls._load_from_file(reference, f)
-        return _object
+    def _set_up_stage(self):
+        # If staging is already set up, then there's nothing to do.
+        if self._stage_dir_and_flock is not None:
+            return
+        # Create a unique stage for the changes to this object, starting with a
+        # copy of the object.
+        all_stages_dir = os.path.join('.wad', 'stage', self._type)
+        try:
+            os.makedirs(all_stages_dir)
+        except OSError:
+            pass
+        stage_dir = tempfile.mkdtemp(dir=all_stages_dir)
+        if self._reference is not None:
+            # Copy over the contents: I would like to be using
+            # shutil.copytree(), but it doesn't like it if the destination
+            # directory already exists.
+            for dirpath, dirnames, filenames in os.walk(self._reference_dir()):
+                relative_dirpath = os.path.relpath(dirpath, self._reference_dir())
+                for dirname in dirnames:
+                    os.makedirs(os.path.join(self._reference_dir(), relative_dirpath, dirname))
+                for filename in filenames:
+                    shutil.copy(
+                        os.path.join(dirpath, filename),
+                        os.path.join(self._reference_dir(), relative_dirpath, filename)
+                    )
+        # The lock lets 'wad status' know that this staging directory is still
+        # in use - on the other hand, if another process acquires this lock,
+        # waits a few moments, and the directory is still present, then it is
+        # likely that there is a programming error resulting in leaked stages.
+        # TODO - every time wad is invoked, check for these leaked stages.
+        lock_filename = os.path.join(stage_dir, 'lock')
+        flock = open(lock_filename, 'w')
+        try:
+            fcntl.flock(flock, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except IOError:
+            raise UnreachableError('{} is locked.'.format(lock_filename)) #  TODO?
+        else:
+            self._stage_dir_and_flock = (stage_dir, flock)
+
+    # TODO context manager for...?
+
+    def object_dir(self):
+        if self._stage_dir_and_flock is not None:
+            stage_dir, _ = self._stage_dir_and_flock
+            return stage_dir
+        else:
+            assert self._reference is not None
+            return self._reference_dir()
+
+    def get(self, path):
+        fn = os.path.join(self.object_dir(), key)
+        if os.path.isfile(fn):
+            with open(fn) as f:
+                return f.read()
+        return None
+
+    def set(self, path, value=None, source_filename=None):
+        self._set_up_stage()
+        object_dir_path = os.path.join(self.object_dir(), path)
+        try:
+            os.makedirs(os.path.dirname(object_dir_path))
+        except OSError:
+            pass
+        if value is not None:
+            assert source_filename is None
+            with open(object_dir_path, 'w') as f:
+                f.write(value)
+        elif source_filename is not None:
+            assert value is None
+            shutil.copy(source_filename, object_dir_path)
 
     def store(self):
-        reference = self.get_reference()
-        with open(type(self)._filename(reference), 'w') as f:
-            self._write_to_file(f)
+        # If there are staged changes:
+        if self._stage_dir_and_flock is not None:
+            stage_dir, flock = self._stage_dir_and_flock
+            # first check that required content isn't missing.
+            for path in self._required_contents:
+                path = os.path.join(stage_dir, path)
+                if not os.path.exists(path):
+                    raise Exception('missing path {}'.format(path)) # TODO InternalExceptoin
+            # then move the directory over to the right place.
+            flock.close()
+            os.remove(os.path.join(stage_dir, 'lock'))
+            shutil.move(stage_dir, self._reference_dir())
 
 
 class Topic(WadObject):
+    __metaclass__ = WadObjectRegistry
 
-    def __init__(self, reference, head_commit):
-        self.reference = reference
-        self.head_commit = head_commit
-
-    def get_reference(self):
-        return self.reference
-
-    @classmethod
-    def _check_reference(cls, reference):
-        if re.search(r'^T:[a-z_]+$', reference) is None:
-            raise Exception('"{}" is not a valid topic reference.'.format(reference))
-
-    @classmethod
-    def _load_from_file(cls, reference, f):
-        (head,) = f.readlines()
-        head_commit = Commit.load(head)
-        if head_commit is None:
-            raise Exception("{}, pointed at by {}, doesn't exist!".format(head, reference))
-        return Topic(reference, head_commit)
-
-    def _write_to_file(self, f):
-        f.write(self.head_commit.get_reference())
+    _type = 'topic'
+    _required_contents = {
+        'description',
+        'head'
+    }
 
 
 def get_head():
@@ -113,14 +200,15 @@ def get_head():
 
 
 def new_topic(name, starting_from_commit=None): # TODO: and 'starting from' argument
-    if Topic.load(name) is not None:
+    topic = Topic(name)
+    if topic.does_exist():
         raise UsageException('Topic "{}" already exists.'.format(name))
     # TODO name must be a-z and underscores
+    topic.set('description', 'TODO')
     if starting_from_commit is None:
-        head_commit = look_up_commit(get_head())
-        topic = Topic(name, head_commit)
+        topic.set('head', look_up_commit(get_head()))
     else:
-        topic = Topic(name, starting_from_commit)
+        topic.set('head', starting_from_commit.get_reference())
     topic.store()
     goto(topic.get_reference())
 
@@ -134,16 +222,19 @@ def command_init():
         os.mkdir('.wad')
     except OSError:
         raise UsageException('Directory {} is already a wad.'.format(os.path.abspath('.')))
-    os.mkdir(os.path.join('.wad', 'commits'))
-    os.mkdir(os.path.join('.wad', 'topics'))
-    init_commit = Commit()
-    init_commit.set_description('wad init')
+    init_commit = Commit(None)
+    init_commit.set('description', 'wad init')
     for dirpath, dirnames, filenames in os.walk('.'):
+        # TODO need to do .wad/ignore
         if dirpath.startswith(os.path.join('.', '.wad', '')):
             # Don't include files under '.wad/'.
             continue
         for fn in filenames:
-            init_commit.add_file(os.path.join(dirpath, fn))
+            relative_dirpath = os.path.relpath(dirpath, '.')
+            init_commit.set(
+                os.path.join('tree', relative_dirpath, fn),
+                source_filename=os.path.join(dirpath, fn)
+            )
     init_commit.store()
     new_topic('main', starting_from_commit=init_commit)
 
@@ -202,12 +293,16 @@ def command_new_commit(description):
     # TODO for filename in diff(): commit.add_files(..)
     # do not proceed if nothing to commit
     commit.store()
-    if head.startswith('T:'):
-        topic = Topic.load(head)
-        topic.head_commit = commit
+    # TODO: add a typesregistry for wadobjects, so the scaffolded if isn't necessary
+    _object = WadObject.look_up(head)
+    if isinstance(_object, Topic):
+        topic = _object
+        topic.set('head', commit.get_reference())
         topic.store()
         goto(head)
-    elif head.startswith('C:'):
+    elif isinstance(_object, Commit):
+        # If we're in detached mode, the new head is just whatever we finished
+        # committing.
         goto(commit.get_reference())
     else:
         raise UnreachableException()
@@ -215,119 +310,37 @@ def command_new_commit(description):
 
 # TODO: author
 # for author, need to read a ~/.wadconfig
-class Commit(object):
+class Commit(WadObject):
+    __metaclass__ = WadObjectRegistry
 
-    def __init__(self, reference=None):
-        self.reference = reference
-
-    def _commit_dir(self):
-        if self.reference == 'stage':
-            return os.path.join('.wad', 'stage')
-        else:
-            return os.path.join('.wad', 'commits', self.reference)
-
-    def _set_up_stage(self):
-        stage_dirname = os.path.join('.wad', 'stage')
-        if self.reference != 'stage':
-            if os.path.isdir(stage_dirname):
-                raise UnreachableException('The stage was not cleaned up.')
-            os.mkdir(stage_dirname)
-            self.reference = 'stage'
-
-    def get_parent(self):
-        # TODO read from reference/parent
-        pass
-
-    def set_parent(self, commit):
-        self._set_up_stage()
-        # TODO write parent into stage
-        pass
-
-    def get_description(self):
-        # TODO see above
-        pass
-
-    def set_description(self, description):
-        self._set_up_stage()
-        with open(os.path.join(self._commit_dir(), 'description'), 'w') as f:
-            f.write(description)
-
-    # After 'add_file' is called once, a Commit is 'staged', meaning that it
-    # needs to be store()d.
-    # TODO: force use with contexts
-    def add_file(self, filename):
-        print 'add_file: {}'.format(filename)
-        self._set_up_stage()
-        path_in_tree = os.path.join(self._commit_dir(), 'tree', filename)
-        try:
-            os.makedirs(os.path.dirname(path_in_tree))
-        except OSError:
-            pass
-        shutil.copyfile(filename, path_in_tree)
+    _type = 'commit'
+    _required_contents = {
+        'description',
+        'tree'
+    }
 
     def store(self):
-        if self.reference != 'stage':
-            raise InternalException() # TODO
-        # Find all files in the commit, sort them, then do a sha1 hash of all
-        # filenames and their contents.
-        paths = []
-        for dirpath, dirnames, filenames in os.walk(self._commit_dir()):
-            for fn in filenames:
-                paths.append(os.path.join(dirpath, fn))
-        paths.sort()
-        _hash = hashlib.sha1()
-        for path in paths:
-            _hash.update('!path!' + base64.b64encode(path))
-            with open(path) as f:
-                for chunk in f.read(1000000):
-                    if chunk == '':
-                        break
-                    _hash.update('!chunk!' + base64.b64encode(chunk))
-        # Move the commit contents to the proper commit directory, and delete
-        # stage.
-        stage_dirname = os.path.join('.wad', 'stage')
-        self.reference = _hash.hexdigest()
-        if os.path.isdir(self._commit_dir()):
-            raise InternalException()
-        shutil.move(stage_dirname, self._commit_dir())
+        # If there are staged changes, figure out what the new reference should
+        # be.
+        if self._stage_dir_and_flock is not None:
+            # Find all files in the commit, sort them, then do a sha1 hash of all
+            # filenames and their contents.
+            paths = []
+            for dirpath, dirnames, filenames in os.walk(self.object_dir()):
+                for fn in filenames:
+                    paths.append(os.path.join(dirpath, fn))
+            paths.sort()
+            _hash = hashlib.sha1()
+            for path in paths:
+                _hash.update('!path!' + base64.b64encode(path))
+                with open(path) as f:
+                    for chunk in f.read(1000000):
+                        if chunk == '':
+                            break
+                        _hash.update('!chunk!' + base64.b64encode(chunk))
+            self._reference = _hash.hexdigest()
         # TODO: might need to think about copying metadata too -- so that mtime is preserved?
-
-    # TODO: uses of os.walk safe against circular links? does it follow them?
-
-    @classmethod
-    def _check_reference(cls, reference):
-        if re.search(r'^C:[0-9a-f]+$', reference) is None:
-            raise Exception('"{}" is not a valid commit reference.'.format(reference))
-
-    # TODO somehow would like to know/check that a Commit (with the same logical data) has not changed its reference
-
-    @classmethod
-    def _load_from_file(cls, reference, f):
-        parent_reference = f.readline().strip() or None
-        description_len = int(f.readline().strip())
-        description = f.read(description_len)
-        f.read(1)
-        # TODO: be lazy - don't read the entire commit contents, yet
-        return Commit(parent_reference, description)
-
-    def _write_to_file(self, f):
-        if self.parent_reference is None:
-            f.write('\n')
-        else:
-            f.write(self.parent_reference + '\n')
-        f.write(str(len(self.description)) + '\n')
-        f.write(self.description + '\n')
-
-
-def look_up_commit(reference):
-    if reference.startswith('C:'):
-        return Commit.load(reference)
-    elif reference.startswith('T:'):
-        topic = Topic.load(reference)
-        assert topic is not None
-        return Commit.load(topic.head_commit.get_reference())
-    else:
-        raise UnreachableException() # TODO
+        super(Commit, self).store()
 
 
 def command_goto(reference):
